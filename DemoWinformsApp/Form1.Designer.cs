72a92b961e4210223a5cd8cc5d7f377e2be9456e
Zachary Watson <zacharywatson1129@gmail.com>
2024-08-21 17:23:13 -0500

    Created the initial app

---
+++ DemoWinformsApp/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DemoWinformsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cambria", 24F);
+            label1.Location = new Point(140, 138);
+            label1.Name = "label1";
+            label1.Size = new Size(236, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Hello World!";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(531, 375);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Demo Winforms App";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ DemoWinformsApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace DemoWinformsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
